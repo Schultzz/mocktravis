0bad0b7163847de190b513a743b00bd7cda91679
LarsM <lam@cphbusiness.dk>
2017-04-18 16:34:21 +0200

    MySQL

--- src/test/java/exercise/UserFacadeTestIT.java
+++ src/test/java/exercise/UserFacadeTestIT.java
@@ -1,12 +1,15 @@
-
 package exercise;
 
 import exercise.realdatabase.UserFacadeRealDB;
 
-
 public class UserFacadeTestIT extends UserFacadeTest {
-  @Override  
-  public IUserFacade makeUserFacade(){
+
+  @Override
+  public IUserFacade makeUserFacade() {
+
+    if (System.getenv("TRAVIS") != null) {
+      return new UserFacadeRealDB("pu_mySql_travis_Integration");
+    }
     return new UserFacadeRealDB("pu_localDB");
   }
 }
